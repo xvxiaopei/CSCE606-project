3c5bb82f7c67e2a6b39de34e3b5e492e3e77e9f3
Xiaopei Xu <xvxiaopei@tamu.edu>
2016-12-02 19:52:43 +0000

    fix some bugs

--- DiseaseDatasetCuration/app/helpers/addquestions_helper.rb
+++ DiseaseDatasetCuration/app/helpers/addquestions_helper.rb
@@ -7,22 +7,21 @@ module AddquestionsHelper
   def search_from_arrayexpress(keywords)
 # Use this for formal version
 
-#    url = 'https://www.ebi.ac.uk/arrayexpress/json/v3/experiments?keywords='+keywords;
-#   p url
-#    uri = URI(url)
-#    response = Net::HTTP.get(uri)
-#    data_hash=JSON.parse(response)
-#    data_result=Hash.new
-#    data_hash["experiments"]["experiment"].each {|value|
-#    data_result[value["accession"]]=value["name"]
-#    }
+    url = 'https://www.ebi.ac.uk/arrayexpress/json/v3/experiments?keywords='+keywords;
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    data_hash=JSON.parse(response)
+    data_result=Hash.new
+    data_hash["experiments"]["experiment"].each {|value|
+    data_result[value["accession"]]=value["name"]
+    }
 
 # Use this for debug
 
-    data_result=Hash.new
-    data_result["E-GEOD-57691"]="Differential gene expression in human abdominal aortic aneurysm and atherosclerosis"
-    data_result["E-MTAB-3175"]="Gene expression study in Positron Emission Tomography (PET) positive abdominal aortic aneurysms"
-    return data_result
+    # data_result=Hash.new
+    # data_result["E-GEOD-57691"]="Differential gene expression in human abdominal aortic aneurysm and atherosclerosis"
+    # data_result["E-MTAB-3175"]="Gene expression study in Positron Emission Tomography (PET) positive abdominal aortic aneurysms"
+     return data_result
+  
   end
- 
 end
