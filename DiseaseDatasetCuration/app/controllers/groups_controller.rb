53b7d71e97bea80ad5dbb33d90b6515185c7f8c3
Xiaopei Xu <xvxiaopei@tamu.edu>
2016-12-06 18:49:33 +0000

    fix some bugs, improve performance

--- DiseaseDatasetCuration/app/controllers/groups_controller.rb
+++ DiseaseDatasetCuration/app/controllers/groups_controller.rb
@@ -90,9 +90,7 @@ class GroupsController < ApplicationController
         @users = find_conditional_users
     
         # update user accuracy fields
-        if !params.has_key?(:page) && !params.has_key?(:query) && !params.has_key?(:order)
-          @users.each { |user| user.update_attribute(:accuracy, user.get_submission_info["accuracy"]) }
-        end
+
         # byebug
     
         if @users == nil
