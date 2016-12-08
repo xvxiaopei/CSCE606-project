ad4c3bfea0b96976a5542e1f185e6fa20c7308ff
Xiaopei Xu <xvxiaopei@tamu.edu>
2016-12-08 00:45:05 +0000

    modify seed

--- DiseaseDatasetCuration/db/seeds.rb
+++ DiseaseDatasetCuration/db/seeds.rb
@@ -13,7 +13,7 @@ require 'csv'
 # Users
 # 102 users
 User.create!(name: "666", email: "666@gmail.com", password: "foobar", password_confirmation: "foobar", admin: true)
-(1..100).each {|i| User.create!(name: "mashuo#{i}", email: "mashuo#{i}@gmail.com", password: "19930521", password_confirmation: "19930521", admin: false)}
+
 
 # Groups
 
@@ -26,9 +26,9 @@ _gadmin3=User.create!(name: "flyer3", email: "flyer3@gmail.com", password: "foob
 password_confirmation: "foobar", admin: true, group_admin: true)
 
 # 3 groups
-_group1=Group.create!(name: "TestGroup1", description: "Testing...", admin_uid: 102)
-_group2=Group.create!(name: "TestGroup2", description: "Still testing...", group_level: "undergraduate",admin_uid:103)
-_group3=Group.create!(name: "TestGroup3",admin_uid:104)
+_group1=Group.create!(name: "TestGroup1", description: "Testing...", admin_uid: 2)
+_group2=Group.create!(name: "TestGroup2", description: "Still testing...", group_level: "undergraduate",admin_uid:3)
+_group3=Group.create!(name: "TestGroup3",admin_uid:4)
 
 #debugger
 _gadmin1.groups << _group1
